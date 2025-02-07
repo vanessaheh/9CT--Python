a1863024690436d57307e837b24d8c2f846af577
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-02-07 12:29:13 +1100

    Initial commit

---
+++ python-for-beginners/02 - Print/ask_for_input.py
@@ -0,0 +1,6 @@
+# The input funciton allows you to prompt the user for a value
+# You need to declare a variable to hold the value entered by the user
+name = input('What is your name? ')
+
+print(name)
+print(name)
